bd3e543e1ef875e7b6b1fc535045065f55b0cc0d
marlonanthony <mad1083@gmail.com>
2020-03-25 22:35:46 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import useDebounce from './useDebounce'
+import keys from './config/keys'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState(''),
+    [results, setResults] = useState([]),
+    [isSearching, setIsSearching] = useState(false),
+    debouncedSearchTerm = useDebounce(searchTerm, 500)
+
+  useEffect(() => {
+    if(debouncedSearchTerm){
+      setIsSearching(true)
+      searchCharacters(debouncedSearchTerm).then(res => {
+        setIsSearching(false)
+        setResults(res)
+      })
+    } else {
+      setResults([])
+    }
+  }, [debouncedSearchTerm])
+
+    return (
+      <div className='App'>
+        <input
+          placeholder="Search Marvel Comics"
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+
+        {isSearching && <div>Searching ...</div>}
+
+        {results.map(result => (
+          <div key={result.id}>
+            <h4>{result.title}</h4>
+            <img
+              alt='thumbnail'
+              src={`${result.thumbnail.path}/portrait_incredible.${
+                result.thumbnail.extension
+              }`}
+            />
+          </div>
+        ))}
+      </div>
+  )
 }
 
-export default App;
+async function searchCharacters(search) {
+  try {
+    const query = `apikey=${keys.apiKey}&titleStartsWith=${search}`
+    const res = await fetch(`https://gateway.marvel.com/v1/public/comics?${query}`)
+    const response = await res.json()
+    return response.data.results
+  } catch(err) {
+    console.error(err)
+    return []
+  }
+}
